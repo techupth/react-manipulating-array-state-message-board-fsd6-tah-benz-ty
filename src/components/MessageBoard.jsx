a78372697f7b14d7f74fd5f94cc4a1f1c591ed1c
Wanchana <s.wanchana41@gmail.com>
2024-06-06 13:15:06 +0700

    feat: add onChange, onClick events to handle new message inputs, summit and delete buttons via useState from ReactJS

--- src/components/MessageBoard.jsx
+++ src/components/MessageBoard.jsx
@@ -1,4 +1,30 @@
+import { useState } from "react";
+
 function MessageBoard() {
+  const [inputMessage, setInputMessage] = useState("");
+  const [messageBoard, setMessageBoard] = useState([
+    "Hello all ! This is first message.",
+  ]);
+
+  const handleInputMessage = (event) => {
+    setInputMessage(event.target.value);
+  };
+  const handleSummitButton = (event) => {
+    const newMessages = [...messageBoard];
+    if (inputMessage) {
+      newMessages.push(inputMessage);
+      setMessageBoard(newMessages);
+      setInputMessage("");
+      event.target.reset;
+    }
+  };
+
+  const handleDeleteButton = (messageIndex) => {
+    const newMessages = [...messageBoard];
+    newMessages.splice(messageIndex, 1);
+    setMessageBoard(newMessages);
+  };
+
   return (
     <div className="app-wrapper">
       <h1 class="app-title">Message board</h1>
@@ -9,15 +35,30 @@ function MessageBoard() {
             name="message-text"
             type="text"
             placeholder="Enter message here"
+            onChange={handleInputMessage}
+            value={inputMessage}
           />
         </label>
-        <button className="submit-message-button">Submit</button>
+        <button className="submit-message-button" onClick={handleSummitButton}>
+          Submit
+        </button>
       </div>
       <div class="board">
-        <div className="message">
-          <h1>Hello all ! This is first message.</h1>
-          <button className="delete-button">x</button>
-        </div>
+        {messageBoard.map((message, index) => {
+          return (
+            <div className="message" key={index}>
+              <h1>{message}</h1>
+              <button
+                className="delete-button"
+                onClick={() => {
+                  handleDeleteButton(index);
+                }}
+              >
+                x
+              </button>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
